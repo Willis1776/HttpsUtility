9176e08e0ecf204cf7331c59d80d3847713c448c
Michael Willis <willis@clarity-hd.com>
2021-01-18 17:25:22 -0700

    JsonData Parsing

---
+++ HttpsUtility/EnergyData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace HttpsUtility
+{
+    public class EnergyData
+    {
+        public string Name { get; set; }
+        public string Identifier { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string TotalPhase { get; set; }
+        public string PhaseA { get; set; }
+        public string PhaseB { get; set; }
+        public string PhaseC { get; set; }
+    }
+}
---
+++ HttpsUtility/JsonData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace HttpsUtility
+{
+    public class JsonData
+    {
+        public string ConvertToJson(EnergyData[] powerData)
+        {
+            var unitData = powerData.Skip(1).ToList();
+            var jo = GetJsonObject(unitData, "Usage");
+            return JsonConvert.SerializeObject(jo);
+        }
+
+        private JObject GetJsonObject(object rawData, string sectionName)
+        {
+            var jobject = new JObject {{sectionName, JToken.FromObject(rawData)}};
+            return jobject;
+        }
+    }
+}
